2c0bf0679c3d363d6610d746c34ad85c50ee2c8b
Ivan Galakhov <igalakhov00@stuy.edu>
2019-03-20 09:32:59 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+#include "drawing/display.h"
+#include "drawing/drawer.h"
+#include "matrix/edge_matrix.h"
+#include "matrix/transformation_matrix.h"
+#include "parsing/obj_parser.h"
+#include "parsing/mdl_parser.h"
+#include "drawing/parametric/parametric.h"
+
+int main(){
+    auto d = new Drawer();
+
+    auto m = new EdgeMatrix();
+    auto t = TransformationMatrix::identity();
+
+    auto p = new MDLParser("../script.mdl");
+
+    p->run_file(m, t, d);
+}
